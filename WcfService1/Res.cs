a3acbcdb21df70860c1f405b258f5a8c825acf55
Arman <arman4u@gmail.com>
2018-05-31 23:51:52 +0200

    Add project files.

---
+++ WcfService1/Res.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Web;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace WcfService1
+{
+    [DataContract]
+    [XmlSerializerFormat]
+    [XmlRoot]
+    public class Res
+    {
+        //[DataMember]
+        [XmlAttribute("ID", Form = XmlSchemaForm.Qualified)]
+        public int Id { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public DateTime Birthdate { get; set; }
+    }
+}
